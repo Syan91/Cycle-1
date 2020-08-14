c42ccca7a64ba64974db4c55a18edc7569bab71f
Syan91 <syan199165@gmail.com>
2020-08-14 09:05:53 +0300

    Initial commit

---
+++ src/test/java/ru/netology/stats/StatisticsServiceTest.java
@@ -0,0 +1,79 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsServiceTest {
+    @Test
+    void calculateSum() {
+        StatisticsService service = new StatisticsService();
+
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+
+        long actual = service.calculateSum(purchases);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 7;
+
+        long actual = service.findMax(purchases);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8;
+
+        long actual = service.findMin(purchases);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void calculateAver() {
+        StatisticsService service = new StatisticsService();
+
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
+
+        long actual = service.calculateAver(purchases);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void lowerThanAver() {
+        StatisticsService service = new StatisticsService();
+
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+
+        long actual = service.lowerThanAver(purchases);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void biggerThanAver() {
+        StatisticsService service = new StatisticsService();
+
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+
+        long actual = service.biggerThanAver(purchases);
+
+        assertEquals(expected, actual);
+    }
+        }
