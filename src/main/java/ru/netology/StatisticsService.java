750ad4386b2b27321f268076080141ad0d2dd325
Syan91 <66880451+Syan91@users.noreply.github.com>
2020-08-17 21:32:34 +0300

    Fix

--- src/main/java/ru/netology/StatisticsService.java
+++ src/main/java/ru/netology/StatisticsService.java
@@ -9,15 +9,16 @@ public class StatisticsService {
         }
         return sum;
     }
-    public long calculateAver(long[] purchases) {
+    public double calculateAver(long[] purchases) {
         long sum = 0;
-        long average = 0;
+        double average = 0;
         long month;
         for (long purchase : purchases) {
             sum += purchase;
-            month = purchases.length ;
-            average = sum / month ;
         }
+        month = purchases.length;
+        average = sum / month;
+
         return average;
     }
     public long findMax(long[] purchases) {
@@ -35,7 +36,7 @@ public class StatisticsService {
         int indexOfMin = 0;
         for (int i = 0; i < purchases.length; i++)
         {
-             if (purchases[i] < purchases[indexOfMin])
+             if (purchases[i] <= purchases[indexOfMin])
             {
                 indexOfMin = i;
             }
@@ -43,8 +44,15 @@ public class StatisticsService {
         return indexOfMin;
     }
     public long lowerThanAver(long[] purchases) {
-        long average = 15;
+        long sum = 0;
+        long average;
+        long month;
         int count = 0;
+        for (long purchase : purchases) {
+            sum += purchase;
+        }
+        month = purchases.length;
+        average = sum / month;
         for (long purchase : purchases) {
             if (purchase < average) {
                 count++;
@@ -53,8 +61,15 @@ public class StatisticsService {
         return count;
     }
     public long biggerThanAver(long[] purchases) {
-        long average = 15;
+        long sum = 0;
+        long average;
+        long month;
         int count = 0;
+        for (long purchase : purchases) {
+            sum += purchase;
+        }
+        month = purchases.length;
+        average = sum / month;
         for (long purchase : purchases) {
             if (purchase > average) {
                 count++;
